b4fd375511b380ba5bb30cc486068b33e6c5216d
George Lubenets <g.lubenets@rt-solar.ru>
2023-08-31 09:24:08 +0300

    vueuse added

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,9 @@
+<script lang="ts" setup>
+import { computedEager } from "@vueuse/core";
+//vueuse https://vueuse.org/guide/best-practice.html
+computedEager(() => {});
+</script>
+
 <template>
   <div class="about">
     <h1><q-btn color="primary">This is quasar btn</q-btn></h1>
