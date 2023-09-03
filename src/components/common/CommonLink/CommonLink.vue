9925933797ea4e5de118116072782a07bf9d5139
spokik <tackesi@yandex.ru>
2023-09-03 14:09:19 +0300

    Добавил компонент простой ссылки

---
+++ src/components/common/CommonLink/CommonLink.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { CommonLinkItem } from './types'
+
+const props = defineProps<CommonLinkItem>()
+</script>
+
+<template>
+    <a :href="props.to" class="link" :style="`color: ${props.color}`" target="_blank" rel="">
+        <slot></slot>
+    </a>
+</template>
+
+<style scoped lang="scss">
+.link {
+    color: $grey-2;
+    cursor: pointer;
+}
+</style>
