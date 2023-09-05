1fc6f1c7fe8f7ef62206dd9455626bd25a200ff0
George Lubenets <g.lubenets@rt-solar.ru>
2023-09-05 21:03:24 +0300

    Вынес в отдельную модалку RegistrationModal

---
+++ src/components/modals/RegistrationModal/RegistrationModal.vue
@@ -0,0 +1,100 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import ModalWrapper from "../../ModalWrapper/ModalWrapper.vue";
+const options: Array<string> = ["Google", "Facebook", "Twitter", "Apple", "Oracle"];
+const text = ref<string>("");
+</script>
+
+<template>
+  <ModalWrapper :show="true">
+    <template v-slot:header>
+      <h4>Добавить данные</h4>
+    </template>
+
+    <template v-slot:subHeader>
+      <span class="label-text">Введите свои данные для регистрации</span>
+    </template>
+
+    <template v-slot:form>
+      <q-input
+        outlined
+        class="fit q-mb-sm"
+        input-class="q-input--form"
+        label="Ф.И.О.*"
+        borderless
+        color="primary"
+        :model-value="text"
+      ></q-input>
+
+      <q-input
+        outlined
+        class="fit q-mb-sm"
+        input-class="q-input--form"
+        label="Телефон*"
+        borderless
+        :modelValue="''"
+      ></q-input>
+
+      <q-input
+        outlined
+        class="fit q-mb-sm"
+        input-class="q-input--form"
+        label="Почта*"
+        borderless
+        :modelValue="''"
+      ></q-input>
+
+      <q-select
+        input-class="q-select--form"
+        label="Регион*"
+        outlined
+        class="fit q-mb-sm"
+        :options="options"
+        :modelValue="'Волгоградская область'"
+      />
+
+      <q-select
+        input-class="q-select--form"
+        label="Специализация*"
+        outlined
+        class="fit q-mb-sm"
+        :options="options"
+        :modelValue="'Школьники'"
+      />
+
+      <q-select
+        class="fit q-mb-sm"
+        input-class="q-select--form"
+        label="Кем Вы являетесь*"
+        outlined
+        :options="options"
+        :modelValue="'Психолог'"
+      />
+
+      <q-input
+        outlined
+        class="fit q-mb-sm"
+        input-class="q-input--form"
+        label="Пароль*"
+        borderless
+        :modelValue="''"
+      ></q-input>
+
+      <q-input
+        outlined
+        class="fit q-mb-sm"
+        input-class="q-input--form"
+        label="Подтверждение пароля*"
+        borderless
+        :modelValue="''"
+      ></q-input>
+    </template>
+
+    <template v-slot:footer>
+      <q-btn label="Регистрация" class="q-btn--form" color="primary"></q-btn>
+      <q-btn class="q-ml-sm q-btn--form" flat :ripple="false" color="grey-1" label="Закрыть"></q-btn>
+    </template>
+  </ModalWrapper>
+</template>
+
+<style lang="scss" scoped></style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 import TheHeader from "@/components/common/TheHeader/TheHeader.vue";
 import TheFooter from "@/components/common/TheFooter/TheFooter.vue";
-import { ref, computed } from "vue";
+import { computed } from "vue";
 import { useRoute } from "vue-router";
-import ModalWrapper from "@/components/ModalWrapper/ModalWrapper.vue";
 
 const route = useRoute();
 
@@ -12,11 +11,6 @@ const pageBackground = computed(() => {
 
   return `background: $background-${backgroundName})`;
 });
-
-const options : Array<String> = [
-        'Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'
-      ];
-const text = ref<String>('')
 </script>
 
 <template>
@@ -24,47 +18,9 @@ const text = ref<String>('')
     <TheHeader />
     <q-page-container :style="pageBackground">
       <q-page style="padding-top: 60px" class="q-pa-md">
-        {{ text }}
         <router-view />
       </q-page>
     </q-page-container>
     <TheFooter />
-    <ModalWrapper :show="true">
-      <template v-slot:header>
-        <h4>
-          Добавить данные
-        </h4>
-      </template>
-
-      <template v-slot:subHeader>
-        <span class="label-text">Введите свои данные для регистрации</span>
-      </template>
-      
-      <template v-slot:form>
-        
-        <label class="q-input--form-label">Ф.И.О.*</label>
-        <q-input input-class="q-input--form" borderless color="primary" v-model:modelValue="text"></q-input>
-        <label class="q-input--form-label">Телефон*</label>
-        <q-input input-class="q-input--form" borderless :modelValue="''"></q-input>
-        <label class="q-input--form-label">Почта*</label>
-        <q-input input-class="q-input--form" borderless :modelValue="''"></q-input>
-        <label class="q-input--form-label">Регион*</label>
-        <q-select input-class="q-select--form" outlined :options="options" :modelValue="'Волгоградская область'" />
-        <label class="q-input--form-label">Специализация*</label>
-        <q-select input-class="q-select--form" outlined :options="options" :modelValue="'Школьники'" />
-        <label class="q-input--form-label">Кем Вы являетесь*</label>
-        <q-select input-class="q-select--form" outlined :options="options" :modelValue="'Психолог'" />
-        <label class="q-input--form-label">Пароль*</label>
-        <q-input input-class="q-input--form" borderless :modelValue="''"></q-input>
-        <label class="q-input--form-label">Подтверждение пароля*</label>
-        <q-input input-class="q-input--form" borderless :modelValue="''"></q-input>
-
-      </template>
-
-      <template v-slot:footer>
-        <q-btn label="Регистрация" class="q-btn--form"></q-btn>
-        <q-btn flat :ripple="false" color='grey-1' label="Закрыть"></q-btn>
-      </template>
-    </ModalWrapper>
   </q-layout>
 </template>
