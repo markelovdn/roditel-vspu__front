195a7c33e04e31e4c6fd473656b2f2cb872fa7d2
Dmitriy <markelovdn@gmail.com>
2023-09-06 11:25:53 +0300

    Лайаут модального окна, регистрационная форма

--- src/components/modals/RegistrationModal/RegistrationModal.vue
+++ src/components/modals/RegistrationModal/RegistrationModal.vue
@@ -1,29 +1,57 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import ModalWrapper from "../../ModalWrapper/ModalWrapper.vue";
+import axios  from "axios";
+
 const options: Array<string> = ["Google", "Facebook", "Twitter", "Apple", "Oracle"];
-const text = ref<string>("");
+const subHeader: string = 'Введите свои данные для регистрации';
+const header: string = 'Добавить данные';
+
+const show = ref<boolean>(true);
+const isPwd = ref<boolean>(true);
+
+const data = ref<object>({
+  name: ref<string>(''),
+  phone: ref<string>(''),
+  email: ref<string>(''),
+  specialization_id: ref<number>(),
+  profession_id: ref<number>(),
+  password: ref<string>(''),
+  password_confirmation: ref<string>(''),
+})
+
+const sentData = async (data: object) => {
+      await axios.post('https://markelovdn.ru/api/register', {
+        data
+      }).then((response) => {
+        localStorage.setItem('token', response.data.access_token)
+        console.log(response.data.user)
+
+      }).catch((errors) => {
+        console.log(errors)
+      })
+}
 </script>
 
 <template>
-  <ModalWrapper :show="true">
+  <ModalWrapper :show="show">
     <template v-slot:header>
-      <h4>Добавить данные</h4>
+      {{ header }}
     </template>
 
     <template v-slot:subHeader>
-      <span class="label-text">Введите свои данные для регистрации</span>
+      {{ subHeader }}
     </template>
-
+    
     <template v-slot:form>
-      <q-input
+        <q-input
         outlined
         class="fit q-mb-sm"
         input-class="q-input--form"
         label="Ф.И.О.*"
         borderless
         color="primary"
-        :model-value="text"
+        v-model="data.name"
       ></q-input>
 
       <q-input
@@ -31,8 +59,9 @@ const text = ref<string>("");
         class="fit q-mb-sm"
         input-class="q-input--form"
         label="Телефон*"
+        mask="+7 (###) ### ####"
         borderless
-        :modelValue="''"
+        v-model="data.phone"
       ></q-input>
 
       <q-input
@@ -41,17 +70,8 @@ const text = ref<string>("");
         input-class="q-input--form"
         label="Почта*"
         borderless
-        :modelValue="''"
+        v-model="data.email"
       ></q-input>
-
-      <q-select
-        input-class="q-select--form"
-        label="Регион*"
-        outlined
-        class="fit q-mb-sm"
-        :options="options"
-        :modelValue="'Волгоградская область'"
-      />
 
       <q-select
         input-class="q-select--form"
@@ -59,7 +79,7 @@ const text = ref<string>("");
         outlined
         class="fit q-mb-sm"
         :options="options"
-        :modelValue="'Школьники'"
+        v-model="data.specialization_id"
       />
 
       <q-select
@@ -68,31 +88,58 @@ const text = ref<string>("");
         label="Кем Вы являетесь*"
         outlined
         :options="options"
-        :modelValue="'Психолог'"
+        v-model="data.profession_id"
       />
 
       <q-input
-        outlined
-        class="fit q-mb-sm"
-        input-class="q-input--form"
-        label="Пароль*"
-        borderless
-        :modelValue="''"
-      ></q-input>
+      outlined
+      class="fit q-mb-sm"
+      label="Пароль*"
+      v-model="data.password" 
+      :type="isPwd ? 'password' : 'text'"
+      >
+      <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
 
       <q-input
-        outlined
-        class="fit q-mb-sm"
-        input-class="q-input--form"
-        label="Подтверждение пароля*"
-        borderless
-        :modelValue="''"
-      ></q-input>
+      outlined
+      class="fit q-mb-sm"
+      label="Подтвердите пароль*"
+      v-model="data.password_confirmation" 
+      :type="isPwd ? 'password' : 'text'"
+      >
+      <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
     </template>
 
     <template v-slot:footer>
-      <q-btn label="Регистрация" class="q-btn--form" color="primary"></q-btn>
-      <q-btn class="q-ml-sm q-btn--form" flat :ripple="false" color="grey-1" label="Закрыть"></q-btn>
+      <q-btn 
+      label="Регистрация" 
+      class="q-btn--form" color="primary"
+      @click="sentData(data)"
+      ></q-btn>
+      
+      <q-btn
+      label="Закрыть"
+      class="q-ml-sm q-btn--form"
+      flat
+      :ripple="false"
+      color="grey-1"
+      @click="show = !show"
+      ></q-btn>
+
     </template>
   </ModalWrapper>
 </template>
