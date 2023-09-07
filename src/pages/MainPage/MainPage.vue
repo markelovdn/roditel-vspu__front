c440b58ccc0e33a47b14aaeb7a06f235b978ebca
Spokik <tackesi@yandex.ru>
2023-09-07 12:45:54 +0300

    Базовая разметка компонентов

--- src/pages/MainPage/MainPage.vue
+++ src/pages/MainPage/MainPage.vue
@@ -1,16 +1,11 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import ShortProgramModal from '@/components/modals/WebinarModal/ShortProgram.vue'
+import ShortProgramModal from "@/components/modals/WebinarModal/ShortProgram.vue";
 const showProgramModal = ref(true);
 </script>
 
 <template>
-<ShortProgramModal v-if="showProgramModal" @close="showProgramModal = !showProgramModal"></ShortProgramModal>
-  <p v-for="n in 15" :key="n">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore vitae
-    odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi perferendis
-    voluptate?
-  </p>
+  <ShortProgramModal v-if="showProgramModal" @close="showProgramModal = !showProgramModal"></ShortProgramModal>
 </template>
 
 <style scoped></style>
