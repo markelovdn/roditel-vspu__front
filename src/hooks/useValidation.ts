f8f3558387b2404e291ec60ff2a4eb34c5c108ee
Dmitriy <markelovdn@gmail.com>
2023-09-10 19:05:13 +0300

    add email and phone validator

--- src/hooks/useValidation.ts
+++ src/hooks/useValidation.ts
@@ -2,11 +2,14 @@ import useVuelidate, { type ValidationArgs } from "@vuelidate/core";
 
 import { computedEager } from "@vueuse/core";
 import { computed, type Ref } from "vue";
-import { helpers, required } from "@vuelidate/validators";
+import { email, helpers, minLength, required } from "@vuelidate/validators";
 
 export type TRule<T> = ValidationArgs<T>;
 
 export const requiredValidator = helpers.withMessage("Обязательное поле", required);
+export const emailValidator = helpers.withMessage("Введен не верный формат email адреса", email);
+export const minLengthValidator = helpers.withMessage(`Минимальная длина: 11 цифр`, minLength(17))
+export const splitName = (value: string) => value.split(' ').length >= 3;
 
 export function useValidation<T extends {}>(data: Ref<T>, rules: TRule<T>) {
   const getRules = computed(() => rules);
