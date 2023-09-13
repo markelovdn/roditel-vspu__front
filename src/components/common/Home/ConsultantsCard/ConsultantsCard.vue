bed768aa0b8cb0435c8f7d83e1efbf75d0377bd5
Сергей <tackesi@yandex.ru>
2023-09-13 17:32:32 +0300

    Доделал ConsultantsCard.vue без модалки

--- src/components/common/Home/ConsultantsCard/ConsultantsCard.vue
+++ src/components/common/Home/ConsultantsCard/ConsultantsCard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref, onMounted } from 'vue';
 import type { Consultant } from './type';
 
 
@@ -7,16 +7,32 @@ const props = defineProps<{
     consultant: Consultant
 }>()
 
+const description = ref<HTMLElement | null>(null)
+const isFullShow = ref(false)
+
+onMounted(() => {
+    if (description.value && description.value?.getBoundingClientRect().height > 73) {
+        isFullShow.value = true
+    }
+})
+
+
+
+
 </script>
 
 <template>
     <div class="card">
         <q-img class="card__photo" :src="props.consultant.photo" />
         <h5 class="card__name">{{ consultant.user.surName }}</h5>
-        <span class="card__description">{{ consultant.description }}</span>
+        <div ref="description" class="card__description" :class="{ card__description_hide: isFullShow }"><span>{{
+            consultant.description }}</span></div>
+        <div v-if="isFullShow" class="card__next"><span>читать далее</span></div>
         <q-btn color="yellow card__button">
             <div class="btn__label">Задать вопрос специалисту</div>
         </q-btn>
+
+
     </div>
 </template>
 
@@ -24,11 +40,14 @@ const props = defineProps<{
 
 <style lang="scss" scoped>
 .card {
+    position: relative;
     padding: 25px;
     border-radius: 20px;
     background: $background-secondary;
     width: 360px;
     height: 515px;
+    font-size: 16px;
+    user-select: none; //Для нормального скролла мышкой
 
     &__photo {
         height: 214px;
@@ -40,16 +59,32 @@ const props = defineProps<{
     }
 
     &__description {
-
         margin-top: 24px;
-        font-size: 16px;
         font-weight: 400;
         line-height: 132%;
+
+        &_hide {
+            height: 65px;
+            overflow-y: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 3;
+            -webkit-box-orient: vertical;
+        }
+    }
+
+    &__next {
+        margin-top: 10px;
+        color: $blue-accent;
+        font-weight: 400;
+        line-height: 132%;
+        cursor: pointer;
     }
 
     &__button {
-        margin-top: 28px;
-        width: 100%;
+        width: calc(100% - 25px*2);
+        position: absolute;
+        bottom: 20px;
     }
 
 }
