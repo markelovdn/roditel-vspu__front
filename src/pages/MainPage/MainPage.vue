12d37e4c0b06a35311c5442db7e7c9f59fe9e462
Dmitriy <markelovdn@gmail.com>
2023-09-15 11:54:46 +0300

    login form finish

--- src/pages/MainPage/MainPage.vue
+++ src/pages/MainPage/MainPage.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
 import RegistrationModal from "@/components/modals/RegistrationModal/RegistrationModal.vue";
-import LoginModal from "@/components/modals/RegistrationModal/RegistrationModal.vue";
+import LoginModal from "@/components/modals/LoginModal/LoginModal.vue";
 import { ref } from "vue";
 
-const showModal = ref(false);
+const showRegistrationModal = ref(false);
+const showLoginModal = ref(false);
 </script>
 
 <template>
-  <q-btn @click="showModal = true">show modal</q-btn>
-  <RegistrationModal v-if="showModal" @close="showModal = false"></RegistrationModal>
+  <q-btn @click="showRegistrationModal = true">Регистраци</q-btn>
+  <q-btn @click="showLoginModal = true">Вход</q-btn>
+  <RegistrationModal v-if="showRegistrationModal" @close="showRegistrationModal = false"></RegistrationModal>
+  <LoginModal v-if="showLoginModal" @close="showLoginModal = false"></LoginModal>
+
   <p v-for="n in 15" :key="n">
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore vitae
     odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi perferendis
