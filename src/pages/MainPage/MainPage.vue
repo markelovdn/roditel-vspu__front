53163da5aec8c6f56a9aa7d76d1e235d10f94062
George Lubenets <g.lubenets@rt-solar.ru>
2023-09-17 17:32:13 +0300

    LoginModal login btn isValid

--- src/pages/MainPage/MainPage.vue
+++ src/pages/MainPage/MainPage.vue
@@ -1,17 +1,6 @@
-<script setup lang="ts">
-import LoginModal from "@/components/modals/LoginModal/LoginModal.vue";
-import { ref } from "vue";
-
-
-const showLoginModal = ref(false);
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  
-  <q-btn @click="showLoginModal = true">Вход</q-btn>
-  
-  <LoginModal v-if="showLoginModal" @close="showLoginModal = false"></LoginModal>
-
   <p v-for="n in 15" :key="n">
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore vitae
     odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi perferendis
