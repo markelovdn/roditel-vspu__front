5197e0620cd6ab5fc47ae250b4091ce9987c677c
Dmitriy <markelovdn@gmail.com>
2023-09-21 06:44:19 +0300

    type TWebinar

--- src/components/modals/WebinarModal/types.ts
+++ src/components/modals/WebinarModal/types.ts
@@ -1,10 +1,10 @@
 export type TWebinarResponse = {
   id: number | null,
   title: string | undefined,
-  questions: [
-    {id: number | null,
-      questionText: string | undefined,}
-  ]
+  questions: {
+      id: number | null,
+      questionText: string | undefined
+  }[]
 };
 
 //Переделать получение с бэка question_text
