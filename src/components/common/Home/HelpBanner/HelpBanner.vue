d55a838f9ce945e3182e9fe5c1afd2c31bfdb611
Сергей <tackesi@yandex.ru>
2023-09-23 15:58:37 +0300

    Временно

---
+++ src/components/common/Home/HelpBanner/HelpBanner.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const content = ['Родителям или законным представителям детей дошкольного возраста, не посещающих дошкольные образовательные организации', 'Родителям или законным представителям, чьи дети находятся на семейном обучении',
+    'Родителям или законным представителям детей с ограниченными возможностями здоровья (ОВЗ) и инвалидностью,в первую очередь раннего возраста',
+    'Родителям или законным представителям нуждающимся в помощи при воспитании детей, посещающих образовательныеорганизации, но имеющих различные проблемы в поведении, развитии, социализации',
+    'Гражданам, желающим принять на воспитание в свои семьи детей, оставшихся без попечения родителей',
+
+]
+
+</script>
+
+<template>
+    <div class="banner-container ">
+        <div class="title">
+            <h2>Кому оказывается бесплатная помощь?</h2>
+        </div>
+        <div class="content">
+            <div v-for="text in content" class="content__item" :key="text">
+                <div class="content__item_point"></div>
+                <div class="content__item_text">{{ text
+                }}</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.banner-container {
+    margin-top: 97px;
+    width: 100%;
+    border-radius: 30px;
+    min-height: 767px;
+    background: #F5F5F5;
+    padding: 79px 55px 79px 79px;
+
+    display: flex;
+    justify-content: space-around;
+}
+
+.title {
+    width: 50%;
+}
+
+.content {
+    width: 50%;
+    display: grid;
+    row-gap: 44px;
+
+
+    &__item {
+        display: grid;
+        grid-template-columns: auto auto;
+        gap: 0 24px;
+
+        &_text {
+            color: $Text-color;
+            font-size: 20px;
+            line-height: 120%;
+            /* 24px */
+        }
+
+        &_point {
+            margin-top: 8px;
+            width: 8px;
+            height: 8px;
+            border-radius: 50%;
+            background-color: $yellow;
+        }
+    }
+}
+</style>
--- src/pages/MainPage/MainPage.vue
+++ src/pages/MainPage/MainPage.vue
@@ -4,7 +4,9 @@ import MainBanner from '@/components/common/Home/MainBanner/MainBanner.vue'
 import ConsultantsSlider from '@/components/common/Home/ConsultantsSlider/ConsultantsSlider.vue'
 import WebinarsPreview from '@/components/common/Home/WebinarsPreview/WebinarsPreview.vue'
 import ServiceDeliveryFormat from '@/components/common/Home/ServiceDeliveryFormat/ServiceDeliveryFormat.vue'
+import HelpBanner from '@/components/common/Home/HelpBanner/HelpBanner.vue'
 import ShortProgramModal from '@/components/modals/WebinarModal/ShortProgram.vue'
+
 
 const showProgramModal = ref(false);
 const showModal = ref(false);
@@ -17,7 +19,10 @@ const showModal = ref(false);
     <WebinarsPreview />
     <ServiceDeliveryFormat />
 
-    <div>Help</div>
+    <HelpBanner />
+
+    <ShortProgramModal v-if="showProgramModal" @close="showProgramModal = !showProgramModal"></ShortProgramModal>
+
     <q-btn @click="showModal = true">show modal</q-btn>
     <RegistrationModal v-model:show-modal="showModal"></RegistrationModal>
 
