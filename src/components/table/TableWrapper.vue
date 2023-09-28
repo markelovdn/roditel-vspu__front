756cf3688e587e85298784be67ff3f7cc11f47b4
Spokik <tackesi@yandex.ru>
2023-09-28 20:00:36 +0300

    small fix

---
+++ src/components/table/TableWrapper.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="table-wrapper">
+    <div class="table-wrapper__header">
+      <h5>{title}</h5>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.table-wrapper {
+  background-color: $background-table;
+  height: 700px;
+  margin-left: 110px;
+
+  &__header {
+    display: flex;
+    align-items: center;
+    margin: 15px 35px;
+    height: 80px;
+    background-color: $background-primary;
+  }
+}
+</style>
---
+++ src/pages/UserCabinet/UserCabinet.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+import { TRegistrationRole } from '../../components/modals/RegistrationModal/types';
+import TableWrapper from "@/components/table/TableWrapper.vue";
+</script>
+
+<template>
+  <div class="container">
+    <div class="title flex">
+      <h2>UserName UserName UserName</h2>
+      <span class="title_type">userType</span>
+      -=icon=-
+    </div>
+    <div class="content">
+      <div class="menu">
+        <li>Журналы</li>
+        <li>Заявки</li>
+        <li>Анкеты</li>
+        <li>Персональные данные</li>
+      </div>
+      <div class="table">
+        <TableWrapper />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  margin: 70px;
+}
+
+.title {
+  align-items: end;
+
+  &_type {
+    color: $grey-1;
+    margin: 0 0 0 24px;
+
+    font-size: 28px;
+    font-weight: 700;
+    line-height: 120%;
+    /* 33.6px */
+  }
+}
+
+.content {
+  display: flex;
+}
+
+.menu {
+  width: 300px;
+  border: solid 1px black;
+}
+
+.table {
+  width: 880px;
+}
+</style>
