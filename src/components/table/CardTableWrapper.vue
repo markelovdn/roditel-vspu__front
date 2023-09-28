d1f2146d6a2e7b7923473e5f2e375708bb12b69d
Spokik <tackesi@yandex.ru>
2023-09-29 01:05:17 +0300

    Набросок CardTableWapper

---
+++ src/components/table/CardTableWrapper.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import type { Webinar } from "./CardTableWebinar/types";
+
+const props = defineProps<{
+  items: Webinar[];
+}>();
+</script>
+
+<template>
+  <div v-if="!items.length">Loading...</div>
+  <div v-for="item in items">
+    <slot name="item" v-bind="item"></slot>
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
