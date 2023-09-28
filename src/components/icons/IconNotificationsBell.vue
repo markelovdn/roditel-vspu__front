31471a99525ae729b90db834463f6fb900bd9561
Spokik <tackesi@yandex.ru>
2023-09-29 01:45:56 +0300

    Добавил стилей

---
+++ src/components/icons/IconNotificationsBell.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+defineProps<{
+  count: number;
+}>();
+</script>
+
+<template>
+  <div class="icon-wrapper">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="25" viewBox="0 0 24 25" fill="none">
+      <path
+        d="M12.0001 2.8999C8.02368 2.8999 4.80013 6.12345 4.80013 10.0999V14.4028L3.9516 15.2514C3.60841 15.5946 3.50574 16.1107 3.69148 16.5591C3.87721 17.0075 4.31478 17.2999 4.80013 17.2999H19.2001C19.6855 17.2999 20.1231 17.0075 20.3088 16.5591C20.4945 16.1107 20.3919 15.5946 20.0487 15.2514L19.2001 14.4028V10.0999C19.2001 6.12345 15.9766 2.8999 12.0001 2.8999Z"
+        fill="#F7B70B" />
+      <path
+        d="M12.0001 22.0999C10.0119 22.0999 8.4001 20.4881 8.4001 18.4999H15.6001C15.6001 20.4881 13.9883 22.0999 12.0001 22.0999Z"
+        fill="#F7B70B" />
+    </svg>
+    <div v-if="count" class="count">
+      {{ count }}
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.icon-wrapper {
+  position: relative;
+}
+.count {
+  width: 16px;
+  height: 10px;
+  border-radius: 40% /50%;
+  background-color: var(--error);
+  color: var(--white-back);
+  position: absolute;
+  right: -2px;
+  top: 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 8px;
+  font-weight: 700;
+}
+</style>
