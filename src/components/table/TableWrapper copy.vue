1185fbc487b2e4df689b8212ae57581102a5a14a
Spokik <tackesi@yandex.ru>
2023-09-29 01:05:17 +0300

    Набросок CardTableWapper

--- src/components/table/TableWrapper copy.vue
+++
@@ -1,8 +0,0 @@
-import { TRegistrationRole } from '../../components/modals/RegistrationModal/types';
-<script setup lang="ts"></script>
-
-<template>
-  <div></div>
-</template>
-
-<style scoped lang="scss"></style>
