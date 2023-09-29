e9fcd8a3e058ab7923e3528d7156c8ffb28d7af2
George Lubenets <g.lubenets@rt-solar.ru>
2023-09-29 21:01:24 +0300

    Переделал структуру и принцип работы с tableWrapper

--- src/components/table/CardTableWebinar/types.ts
+++
@@ -1,8 +0,0 @@
-export interface Webinar {
-  imageUrl: string;
-  title: string;
-  category: string; //* Добавить категории
-  lecturer?: string;
-  cost: "Бесплатно" | number;
-  date: string; //* Временно, пока непонятно в каком формате это будет
-}
