b28ea4cd3227506290a8b4a36bf1cfcdbb2fa1da
George Lubenets <g.lubenets@rt-solar.ru>
2023-10-01 20:46:30 +0300

    fix collectionsStore and env url path

--- src/stores/collectionsStore.ts
+++ src/stores/collectionsStore.ts
@@ -4,38 +4,35 @@ import { computed } from "@vue/reactivity";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
-export const useCollectionsStore = defineStore("CollectionsStore", () => {
+export const useCollectionsStore = defineStore("collectionsStore", () => {
   const regions = ref<TCollectionItem[]>();
   const specializations = ref<TCollectionItem[]>();
   const professions = ref<TCollectionItem[]>();
 
   function requestRegions() {
-    return collectionsApi.getRegions();
+    collectionsApi.getRegions().then((resp) => (regions.value = resp.data.data));
   }
   function requestSpecializations() {
-    return collectionsApi.getSpecializations();
+    collectionsApi.getSpecializations().then((resp) => (specializations.value = resp.data.data));
   }
   function requestProfessions() {
-    return collectionsApi.getProfessions();
+    collectionsApi.getProfessions().then((resp) => (professions.value = resp.data.data));
   }
-  const getRegions = computed(
-    () =>
-      regions.value?.map((item: TCollectionItem) => {
-        return { label: item.title, value: item.id };
-      }),
-  );
-  const getSpecializations = computed(
-    () =>
-      specializations.value?.map((item: TCollectionItem) => {
-        return { label: item.title, value: item.id };
-      }),
-  );
-  const getProfessions = computed(
-    () =>
-      professions.value?.map((item: TCollectionItem) => {
-        return { label: item.title, value: item.id };
-      }),
-  );
+  const getRegions = computed(() => {
+    return regions.value?.map((item: TCollectionItem) => {
+      return { label: item.title, value: item.id };
+    });
+  });
+  const getSpecializations = computed(() => {
+    return specializations.value?.map((item: TCollectionItem) => {
+      return { label: item.title, value: item.id };
+    });
+  });
+  const getProfessions = computed(() => {
+    return professions.value?.map((item: TCollectionItem) => {
+      return { label: item.title, value: item.id };
+    });
+  });
   return {
     regions,
     specializations,
