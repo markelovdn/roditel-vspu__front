7c678d7ab72ba9f1a24297cac98fb6e4b8639e02
Сергей <tackesi@yandex.ru>
2023-10-04 20:03:36 +0300

    Загрузка юзера при загрузке сторы

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -2,10 +2,18 @@ import { authApi } from "@/api";
 import { TUser, TUserRole } from "@/api/Auth/types";
 import { defineStore } from "pinia";
 import { ref, onMounted } from "vue";
+import { useAuthStore } from "./authStore";
 
 export const useUserStore = defineStore("userStore", () => {
-  // todo добавить проверку на наличие/корректность localStorage.user
-  const user = ref<TUser>(JSON.parse(localStorage.user));
+  const authStore = useAuthStore();
+  const user = ref<TUser>();
+
+  authApi
+    .getUserByToken(authStore.token || "")
+    .then((res) => (user.value = res.data.userData))
+    .catch((err) => {
+      console.log(err);
+    });
 
   function setUser(User: TUser) {
     user.value = User;
