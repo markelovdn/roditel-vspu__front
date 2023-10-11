26968d0cd2ba4eb19cbbc35d27f4c210a61cdf58
Сергей <tackesi@yandex.ru>
2023-10-12 01:02:17 +0300

    попытка сделать фильтр

--- src/components/common/Home/WebinarsPreview/WebinarsFilter.vue
+++ src/components/common/Home/WebinarsPreview/WebinarsFilter.vue
@@ -1,11 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { storeToRefs } from "pinia";
+import { onMounted, ref } from "vue";
+
+import { useCollectionsStore } from "@/stores/collectionsStore";
+
+const collectionsStore = useCollectionsStore();
+const { getSpecializationsWithAll: optionsSpecializations } = storeToRefs(collectionsStore);
+const specializationId = ref(0);
+
+onMounted(() => {
+  collectionsStore.requestSpecializations();
+});
+</script>
 
 <template>
-  <div class="filter">filter</div>
+  <div class="filter">
+    <q-select
+      v-model="specializationId"
+      input-class="q-select--form"
+      label="Специализация*"
+      outlined
+      class="fit q-mb-sm"
+      :options="optionsSpecializations"
+      :option-label="(item) => item.label"
+      emit-value
+      map-options />
+  </div>
 </template>
 
 <style lang="scss" scoped>
 .filter {
+  width: 500px;
   background-color: aliceblue;
 }
 </style>
