96146a2da3aeebd016ce1dc8929acda221b4458d
George Lubenets <g.lubenets@rt-solar.ru>
2023-10-15 19:13:34 +0300

    consultants store & api

---
+++ src/api/Consultant/ConsultantApiService.ts
@@ -0,0 +1,13 @@
+import axios from "@/common/axios";
+
+import { TGetConsultantReportsFilter } from "./types";
+
+export class ConsultantApiService {
+  //TODO: указать типы response
+  getReports(consultantId: number | string, filters: TGetConsultantReportsFilter) {
+    const query = new URLSearchParams({ ...filters, page: filters.page.toString() } || {});
+    //TODO: написать функцию принимающуу url и параметры query, на выходе целая строка собранная
+    // учесть то, что URLSearchParams хочет на вход параметры в виде строки, нужен для этого конвертер
+    return axios.get(`/consultant/${consultantId}/reports${query ? "?" + query : ""}`);
+  }
+}
