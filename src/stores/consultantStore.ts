9a5e23a1ac6783e740433e82656183f9a04454d1
George Lubenets <g.lubenets@rt-solar.ru>
2023-10-15 21:15:18 +0300

    listTableWrapper через grids

--- src/stores/consultantStore.ts
+++ src/stores/consultantStore.ts
@@ -7,17 +7,17 @@ import { TGetConsultantReportsFilter } from "@/api/Consultant/types";
 import { useAuthStore } from "./authStore";
 
 export const useConsultantStore = defineStore("consultantStore", () => {
-  const reportsList = ref<Record<string, unknown>[]>([]);
+  const reportsModel = ref<{ data: Record<string, unknown>[]; meta: { currentPage: number; totalPages: number } }>();
   const authStore = useAuthStore();
 
   const consultantId = authStore.getUserId;
   function getReports(filters: TGetConsultantReportsFilter) {
     if (consultantId === undefined) return;
-    consultantApi.getReports(consultantId, filters).then((resp) => (reportsList.value = resp.data.data));
+    consultantApi.getReports(consultantId, filters).then((resp) => (reportsModel.value = resp.data));
   }
 
   return {
-    reportsList,
+    reportsModel,
     getReports,
   };
 });
