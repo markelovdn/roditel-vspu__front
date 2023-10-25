271277760e2b99e86946fd52cd343970362a498f
Сергей <tackesi@yandex.ru>
2023-10-25 16:55:34 +0300

    Исправил buildUrl

---
+++ src/hooks/useParamBuilder .ts
@@ -0,0 +1,20 @@
+import { computed, Ref, toValue } from "vue";
+
+type ParamType = string | number | boolean;
+type ParamsType = Record<string, ParamType>;
+export const useParamBuilder = (params: ParamsType | Ref<ParamsType>) => {
+  return computed(() => {
+    const search = new URLSearchParams();
+    const obj = toValue(params);
+    const entries = Object.entries(obj)
+      .filter((e) => e[1] != null)
+      .filter((e) => (typeof e[1] == "string" ? e[1].length > 0 : true))
+      .sort(([a], [b]) => (a > b ? 1 : a < b ? -1 : 0));
+    entries.forEach(([key, value]) => search.append(key, value.toString()));
+    return search;
+  });
+};
+
+export const useUrlParams = (base: string, params: Readonly<Ref<URLSearchParams>>) => {
+  return computed(() => `${base}?${params.value.toString()}`);
+};
