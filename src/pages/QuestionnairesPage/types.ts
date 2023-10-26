bfafa68dd4bc40617a479798ee0d6c6259fb978c
Dmitriy <markelovdn@gmail.com>
2023-10-26 19:05:47 +0300

    fix array type QuestionnairesPayload

--- src/pages/QuestionnairesPage/types.ts
+++ src/pages/QuestionnairesPage/types.ts
@@ -4,15 +4,11 @@ export type TQuestionnairePayload = {
   title: string;
   description: string;
   answerBefore?: string;
-  questions: {
-    [
-      id: number,
-      text: string,
-      description?: string,
-      type: string,
-      options: {
-        [id: number, text: string];
-      },
-    ];
-  };
+  questions: Array<{
+    id?: number | null;
+    text: string;
+    description?: string;
+    type: string;
+    options: Array<{ id?: number | null; text: string }>;
+  }>;
 };
