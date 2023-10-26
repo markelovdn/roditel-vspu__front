d27c4d26ffb68cc236f929633d3740121f4e05a6
Сергей <tackesi@yandex.ru>
2023-10-26 17:38:32 +0300

    Переделываю useRequestPayload

--- src/hooks/useParamBuilder .ts
+++ src/hooks/useParamBuilder .ts
@@ -1,20 +1,19 @@
-import { computed, Ref, toValue } from "vue";
+import { Ref, toValue, unref } from "vue";
 
 type ParamType = string | number | boolean;
 type ParamsType = Record<string, ParamType>;
+
 export const useParamBuilder = (params: ParamsType | Ref<ParamsType>) => {
-  return computed(() => {
-    const search = new URLSearchParams();
-    const obj = toValue(params);
-    const entries = Object.entries(obj)
-      .filter((e) => e[1] != null)
-      .filter((e) => (typeof e[1] == "string" ? e[1].length > 0 : true))
-      .sort(([a], [b]) => (a > b ? 1 : a < b ? -1 : 0));
-    entries.forEach(([key, value]) => search.append(key, value.toString()));
-    return search;
-  });
+  const search = new URLSearchParams();
+  const obj = toValue(params);
+  const entries = Object.entries(obj)
+    .filter((e) => e[1] != null)
+    .filter((e) => (typeof e[1] == "string" ? e[1].length > 0 : true))
+    .sort(([a], [b]) => (a > b ? 1 : a < b ? -1 : 0));
+  entries.forEach(([key, value]) => search.append(key, value.toString()));
+  return search;
 };
 
-export const useUrlParams = (base: string, params: Readonly<Ref<URLSearchParams>>) => {
-  return computed(() => `${base}?${params.value.toString()}`);
+export const useUrlParams = (base: string, params?: Readonly<Ref<URLSearchParams>> | Ref<string> | string) => {
+  return `${base}?${typeof params == "string" ? params : unref(params as Readonly<Ref<URLSearchParams>>)?.toString()}`;
 };
