b9946fe10219a0012ffb85a40d4a1ce0cb6d81e8
NursultanFront <nursultan020395@gmail.com>
2023-11-14 22:58:58 +0500

    feat: add sidebar, chats and send message, but all mocks

--- src/pages/AccountPage/ParentTabs/QuestionsTab.vue
+++ src/pages/AccountPage/ParentTabs/QuestionsTab.vue
@@ -1,7 +1,35 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ChatSideBarWrapper from "@/components/Chat/ChatSideBarWrapper.vue";
+import ChatWrapper from "@/components/Chat/ChatWrapper.vue";
+import MessageInput from "@/components/Chat/MessageInput.vue";
+</script>
 
 <template>
-  <div>2</div>
+  <div class="question">
+    <div class="question__sidebar"><ChatSideBarWrapper /></div>
+    <div class="question__content">
+      <ChatWrapper />
+      <MessageInput />
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.question {
+  display: flex;
+  gap: 2px;
+  background-color: #e4ebf5;
+  height: 700px;
+  &__sidebar {
+    display: flex;
+    background-color: #ffffff;
+    flex-basis: 35%;
+  }
+  &__content {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    flex-basis: 65%;
+  }
+}
+</style>
