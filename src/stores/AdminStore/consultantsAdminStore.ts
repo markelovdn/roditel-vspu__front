2dbcac4cec8813361ce33fe1999fbb61fe50410a
Dmitriy <markelovdn@gmail.com>
2023-12-19 13:26:48 +0300

    store

---
+++ src/stores/AdminStore/consultantsAdminStore.ts
@@ -0,0 +1,51 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+import { adminsApi } from "@/api";
+import { TConsultantForAdmin, TGetConsultantsForAdminFilter } from "@/api/Admin/types";
+import notify from "@/utils/notify";
+
+export const useConsultantsAdminStore = defineStore("consultantsAdminStore", () => {
+  const consultants = ref<TConsultantForAdmin[]>([]);
+
+  const page = ref({
+    current: 1,
+    max: 1,
+  });
+
+  async function requestAllConsultants(filters: TGetConsultantsForAdminFilter) {
+    await adminsApi.getAllConsultantsForAdmin(filters).then((resp) => {
+      page.value.max = resp.data.meta.last_page;
+      page.value.current = resp.data.meta.current_page;
+      consultants.value = resp.data.data;
+    });
+  }
+
+  async function updateContractNumber(consultantId: number, contractNumber: string) {
+    await adminsApi.updateContractNumber(consultantId, contractNumber).then(() => {
+      const consultantIndex = consultants.value.findIndex((c) => c.consultantId === consultantId);
+      if (consultantIndex !== -1) {
+        consultants.value[consultantIndex].contractNumber = contractNumber;
+      }
+      notify({ type: "positive", message: "Номер договора успешно обновлен" });
+    });
+  }
+
+  async function deleteConsultant(consultantId: number) {
+    await adminsApi.deleteConsultant(consultantId).then(() => {
+      const consultantIndex = consultants.value.findIndex((c) => c.consultantId === consultantId);
+      if (consultantIndex !== -1) {
+        consultants.value.splice(consultantIndex, 1);
+      }
+      notify({ type: "positive", message: "Консультант успешно удален" });
+    });
+  }
+
+  return {
+    consultants,
+    page,
+    requestAllConsultants,
+    updateContractNumber,
+    deleteConsultant,
+  };
+});
---
+++ src/stores/AdminStore/parentedsAdminStore.ts
@@ -0,0 +1,42 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+import { adminsApi } from "@/api";
+import { TGetParentedsForAdminFilter, TParentedForAdmin } from "@/api/Admin/types";
+import notify from "@/utils/notify";
+
+export const useParentedsAdminStore = defineStore("parentedAdminStore", () => {
+  const parenteds = ref<TParentedForAdmin[]>([]);
+
+  const page = ref({
+    current: 1,
+    max: 1,
+  });
+
+  async function requestAllParenteds(filters: TGetParentedsForAdminFilter) {
+    await adminsApi.getAllParentedsForAdmin(filters).then((resp) => {
+      page.value.max = resp.data.meta.last_page;
+      page.value.current = resp.data.meta.current_page;
+      parenteds.value = resp.data.data;
+      console.log(parenteds);
+    });
+  }
+
+  async function deleteParented(parentedId: number) {
+    await adminsApi.deleteConsultant(parentedId).then(() => {
+      const parentedIndex = parenteds.value.findIndex((p) => p.parentedId === parentedId);
+      if (parentedIndex !== -1) {
+        parenteds.value.splice(parentedIndex, 1);
+      }
+      notify({ type: "positive", message: "Родитель успешно удален" });
+    });
+  }
+
+  return {
+    requestAllParenteds,
+    deleteParented,
+    parenteds,
+    page,
+  };
+});
+
