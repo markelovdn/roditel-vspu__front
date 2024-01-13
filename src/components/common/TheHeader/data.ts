1313bf4e2b1bc6bd37adc71c6321071d5c1b1391
Nursultan Kadyrov <nur@roky.rocks>
2024-01-14 00:00:04 +0500

    feat: notify bells

---
+++ src/components/common/TheHeader/data.ts
@@ -0,0 +1,22 @@
+export const notificationRoutes = {
+  consultant: {
+    questionnaire: {
+      text: "Ответ на анкету",
+      link: "my?tabId=questionnaires",
+    },
+    message: {
+      text: "Новое сообщение",
+      link: "my?tabId=applications",
+    },
+  },
+  parent: {
+    questionnaire: {
+      text: "Новая анкета",
+      link: "my?tabId=questionaries",
+    },
+    message: {
+      text: "Новое сообщение",
+      link: "my?tabId=questions",
+    },
+  },
+};
